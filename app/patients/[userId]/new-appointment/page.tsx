2a28727d051cde594987ec4a44d0eb43518f2e7b
Shara_62 <sharabalti62@gmail.com>
2024-07-23 15:10:46 +0500

    first commit

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+import { AppointmentForm } from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+
+const Appointment = async ({ params: { userId } }: SearchParamProps) => {
+  const patient = await getPatient(userId);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+        <section className="mb-12 space-y-4">
+          <h1 className="text-dark-700 text-4xl">HealthCare</h1>
+         
+        </section>
+
+          <AppointmentForm
+            patientId={patient?.$id}
+            userId={userId}
+            type="create"
+          />
+
+          <p className="copyright mt-10 py-12">© 2024 HealthCare</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1500}
+        width={1500}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+};
+
+export default Appointment;
